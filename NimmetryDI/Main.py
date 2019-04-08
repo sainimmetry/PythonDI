f5e35cf41c56b0312f0cb9e48be094018984653e
Sai Pavan <saipavan@nimmetry.com>
2019-04-08 16:36:06 +0530

    Added logging to file
    This is file is updated with logger

--- NimmetryDI/Main.py
+++ NimmetryDI/Main.py
@@ -1,14 +1,20 @@
 import datetime
+import logging
 import time
 
 from com.nimmetry.di.LoadListData import LoadListData
 
+logging.basicConfig(filename="Application.log", format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+                    level=logging.INFO)
+
 ts = time.time()
 
-print('Script Starts at ' + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-username = input('Enter a valid username:: \n')
-password = input('Enter a valid password:: \n')
+logging.info('Script Starts at ' + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+api_url = input('Enter a valid url (Hint:: <http|s>://<url>/api) :: \n')
+username = input('Enter a valid username(Hint:foo) \n')
+password = input('Enter a valid password(Hint: boo) \n')
 
-loadData = LoadListData(user_name=username, password=password)
+send_details = input('Are you sure to proceed (yes/no)? (yes) ::\n')
+loadData = LoadListData(user_name=username, password=password, api_url=api_url)
 
-print('Script ends at ' + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+logging.info('Script ends at ' + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
