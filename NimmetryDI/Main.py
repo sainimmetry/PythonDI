7c45df3cbbd3a2a14de6d8cfd5e06d49f756ca6d
Sai Pavan <saipavan@nimmetry.com>
2019-04-12 12:10:33 +0530

    Updated Modularised Code
    Issues 2,3, 5,9 fixed in updated code

--- NimmetryDI/Main.py
+++ NimmetryDI/Main.py
@@ -1,20 +1,13 @@
-import datetime
 import logging
-import sys
-import time
-
 
 from com.nimmetry.di.LoadListData import LoadListData
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
 
 logging.basicConfig(filename="Application.log", format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
                     level=logging.DEBUG)
 
-ts = time.time()
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-logging.info('Script Starts at ' + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+logging.info('Script Execution Starts  ')
 api_url = input('Enter a valid url (Hint:: <http|s>://<url>/api) :: \n')
 username = input('Enter a valid username(Hint:foo) \n')
 password = input('Enter a valid password(Hint: boo) \n')
@@ -22,10 +15,9 @@ password = input('Enter a valid password(Hint: boo) \n')
 send_details = input('Are you sure to proceed (yes/no)? (yes) ::\n')
 if send_details.lower() == 'yes':
     loadData = LoadListData(user_name=username, password=password, api_url=api_url)
+    loadData.main()
+
 else:
     print('Please re-run and enter valid details')
 
-
-ts1 = time.time()
-
-logging.info('Script ends at ' + datetime.datetime.fromtimestamp(ts1).strftime('%Y-%m-%d %H:%M:%S'))
+logging.info('Script Execution ends')
--- NimmetryDI/com/nimmetry/di/LoadListData.py
+++ NimmetryDI/com/nimmetry/di/LoadListData.py
@@ -1,6 +1,6 @@
 import logging
-from com.nimmetry.utils.Constants import USER_API, LOAD_DOMAIN_DATA, DOMAIN_LIST
 
+from com.nimmetry.utils.Constants import USER_API, LOAD_DOMAIN_DATA, DOMAIN_LIST
 from com.nimmetry.utils.RequestHandler import RequestHandler
 
 logging.basicConfig(filename="Application.log", format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
@@ -11,18 +11,15 @@ class LoadListData:
 
     def __init__(self, user_name, password, api_url):
         try:
-            logging.info('Enters into init Fn().. ')
+            logging.debug('Enters into init Fn().. ')
             self.user_name = user_name
             self.password = password
             self.api_url = api_url
-            logging.info(' Initializing rest_call fn().. ')
-            LoadListData.user_login(user_name=user_name, password=password, api_url=api_url)
-            logging.info('Completes rest_call function')
-            logging.info('Exit from init fn().. ')
+            logging.debug('Exit from init fn().. ')
         except Exception as init_error:
-            logging.exception('Error ' + init_error.__str__())
+            logging.exception('Error while Initialing  ' + init_error.__str__())
         else:
-            logging.info('Successfully completed init Fn() ')
+            logging.debug('Successfully completed init Fn() ')
 
     '''
     Method to get connected to API, fetch Domain and load data
@@ -31,39 +28,31 @@ class LoadListData:
     @return
     '''
 
-
-    def user_login(user_name, password, api_url):
-
+    def user_login(self):
         try:
             # Trying to login into API with User Credentials
-            logging.info('Trying to login into API with User Credentials')
-            login_url = api_url + USER_API
+            logging.debug('Trying to login into API with User Credentials')
+            login_url = self.api_url + USER_API
             logging.debug("Initiating the Rest Call Function with url " + login_url)
-            login_user_data = {"user": {"loginId": user_name, "password": password}}
+            login_user_data = {"user": {"loginId": self.user_name, "password": self.password}}
             # Call Post method from Request handler
-            logging.info('Call Post method from Request handler')
+            logging.debug('Calling Post method from Request handler')
             request_handler = RequestHandler()
             user_response = request_handler.post_request_handler(end_point=login_url, header=login_user_data)
-            access_token = user_response.json()["message"]["token"]
-
-            # Calling domain list and load
-            LoadListData.domain_list_data(api_url=api_url, access_token=access_token, request_object=request_handler)
+            self.access_token = user_response.json()["message"]["token"]
+            return self.access_token
 
         except Exception as rest_call_error:
             logging.exception('Error While calling ' + rest_call_error.__str__())
+            return self.access_token
 
-
-
-
-
-    def domain_list_data(api_url, access_token, request_object):
+    def domain_list_data(self, request_object):
         try:
-            # Domains List
+            #  Get Domains List
             logging.info('Trying to fetch list of domains in api')
-            domain_url = api_url + DOMAIN_LIST
-            # access_domain_token = {'x-access-token': access_token}
+            domain_url = self.api_url + DOMAIN_LIST
             logging.debug("Initiating the Rest Call Function with url " + domain_url)
-            resp1 = request_object.get_request_handler(end_point=domain_url, header=access_token)
+            resp1 = request_object.get_request_handler(end_point=domain_url, header=self.access_token)
             logging.info("\n Successfully Fetched  List of domains")
 
             # load from domain list by fetching the list
@@ -78,26 +67,27 @@ class LoadListData:
             print("list of domains:: \n" + domain_list_dict.__str__())
             user_load_domain_input = input("Enter Id from above list  ::\n")
             for entry in resp1.json()['message']:
-                domain_id_1 = entry['_id']
-                if user_load_domain_input == domain_id_1:
+                self.domain_id_1 = entry['_id']
+                if user_load_domain_input == self.domain_id_1:
                     # load data from selected domain
                     logging.info('loading data from selected domain')
-                    LoadListData.load_data(api_url=api_url, load_domain_input=user_load_domain_input,
-                                           request_object=request_object, access_token=access_token)
+
+                    #   LoadListData.load_data(api_url=self.api_url, load_domain_input=user_load_domain_input,
+                    #                         request_object=request_object, access_token=self.access_token)
                     logging.info('Completed and exit from domain_list_data fn() ')
+                    return self.domain_id_1
 
         except Exception as data_error:
-            logging.exception('Data Error while processing ' + data_error.__str__(), data_error)
+            return self.domain_id_1
+            logging.exception('Data Error while processing ' + data_error.__str__())
 
-
-    def load_data(api_url, load_domain_input, request_object, access_token):
+    def load_data(self, request_object):
         try:
-            logging.info('Enters into load_data fn()')
-            load_domain_url = api_url + LOAD_DOMAIN_DATA + load_domain_input
-            #    load_data_header = {'x-access-token': access_token}
+            logging.debug('Enters into load_data fn()')
+            load_domain_url = self.api_url + LOAD_DOMAIN_DATA + self.domain_id_1
             logging.debug('Enters into Get Request Handler...::' + load_domain_url)
             load_response = request_object.get_request_handler(end_point=load_domain_url,
-                                                               header=access_token)
+                                                               header=self.access_token)
             logging.debug('load data from domain response ::' + load_response.__str__())
             for test in load_response.json()['message']:
                 name = test['name']
@@ -109,5 +99,42 @@ class LoadListData:
                     'Load Name :: ' + name + ' Source Name :: ' + source_name + ' Source TimeStamp :: '
                     + src_timestamp + ' Target:: ' + target_name + ' target ts:: ' + target_timestamp)
 
+            logging.info('data load completed for user specified domain list')
+            return 'Completed'
+
         except Exception as load_data_error:
-            logging.exception('Error while fetching load data ' + load_data_error.__str__(), load_data_error)
+            logging.exception('Error while fetching load data ' + load_data_error.__str__())
+            return 'Failed'
+
+    '''
+    @self
+    @main
+    This method is used to user login API, Get Domain List, get data load from Domain
+    '''
+    def main(self):
+        try:
+            logging.debug('Enter into Main() Fn')
+            logging.info('Process flow initiated ')
+            logging.debug('Entered into main fn().. ')
+            request_handler = RequestHandler()
+
+            access_token = self.user_login()
+            if access_token is not None:
+                domain_id = self.domain_list_data(request_handler)
+            else:
+                logging.info('Please check the Web API Call ')
+
+            if domain_id is not None:
+                run_status = self.load_data(request_handler)
+            else:
+                logging.info('Please check log for further details.. ')
+
+            if run_status == 'Completed':
+                print('Process Completed Successfully ')
+                logging.info('Process Completed Successfully ')
+            else:
+                print('Error in loading data')
+                logging.info('Error in loading data')
+
+        except Exception as Main_Error:
+            logging.execption('Error While Executing Main Fn \n' + Main_Error.__str__())
